70e2b125001746030804bf82b8fa1b39d6a32b61
Felix <FelixinLondon@googlemail.com>
2021-04-25 22:31:20 +0200

    Add dependencies

--- src/components/Sensors/AdoptSensor.jsx
+++ src/components/Sensors/AdoptSensor.jsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React,{useState,useEffect,initialState} from "react";
 import {axiosInstance} from "../../clients/axiosInstance";
 
 export default function AdoptSensor(props) {
   const [Sensors, setSensors] = useState(initialState);
-  useEffect(async () => {
-    axiosInstance
-      .get(`adoptable/${props.group_id}`, {
-        headers: { Authorization: `Bearer ${this.props.jwt}` },
-      })
-      .then((res) => {
-        const groups = res.data;
-        this.setState({
-          isLoaded: true,
-          groups: groups,
-        });
-      });
-
-    setData(result.data);
-  });
+//  useEffect(async () => {
+//    axiosInstance
+//      .get(`adoptable/${props.group_id}`, {
+//        headers: { Authorization: `Bearer ${this.props.jwt}` },
+//      })
+//      .then((res) => {
+//        const groups = res.data;
+//        this.setState({
+//          isLoaded: true,
+//          groups: groups,
+//        });
+//      });
+//
+//    setData(result.data);
+//  });
   return <div></div>;
 }
